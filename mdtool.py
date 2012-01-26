074d675b56d47fa1a6ac4c003c82da3676537d34
Andrew G. Crowell <overkill9999@gmail.com>
2012-01-26 12:05:50 +0000

    Rearranging some code in mdtool.py.

--- mdtool.py
+++ mdtool.py
@@ -24,15 +24,15 @@ import sys
 import markdown # Requires Python-Markdown.
 
 if __name__ == '__main__':
-    md = markdown.Markdown(output_format='html5')
     if len(sys.argv) < 3:
         exit('Usage: ' + os.path.basename(sys.argv[0]) + ' destdir ...files\n')
 
     destdir = sys.argv[1]
     if not os.path.exists(destdir):
         os.makedirs(destdir)
- 
+
     print('- Building...')
+    md = markdown.Markdown(output_format='html5')
     for filename in sys.argv[2:]:
         result = md.convert(open(filename).read())
         dest = os.path.join(destdir, os.path.splitext(filename)[0] + '.html')
